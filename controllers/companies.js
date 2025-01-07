9e16430fe90026a2a6bbf177b4817916fdea06c3
GARajab <mohamed.rajab@ewa.bh>
2025-01-07 14:50:23 +0300

    finish server.js part1

---
+++ controllers/companies.js
@@ -0,0 +1,30 @@
+// companiesList,
+//   newCompanieForm,
+//   createCompanie,
+//   companieDetails,
+//   editCompanie,
+//   updateCompanie,
+//   editCompanieStatusForm,
+//   deleteCompanie,
+const mongoose = require("mongoose");
+const Company = require("../models/company.js");
+
+const companiesList = async (req, res) => {};
+const newCompanieForm = async (req, res) => {};
+const createCompanie = async (req, res) => {};
+const companieDetails = async (req, res) => {};
+const editCompanie = async (req, res) => {};
+const updateCompanie = async (req, res) => {};
+const editCompanieStatusForm = async (req, res) => {};
+const deleteCompanie = async (req, res) => {};
+
+export default {
+  companiesList,
+  newCompanieForm,
+  createCompanie,
+  companieDetails,
+  editCompanie,
+  updateCompanie,
+  editCompanieStatusForm,
+  deleteCompanie,
+};
